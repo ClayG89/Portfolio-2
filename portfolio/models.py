3cd3d1cb499fae774d0ac29783003076e6b065c1
ClayGuess <guessclay@Yahoo.com>
2020-04-17 19:17:09 -0400

    created models

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+class Blogs(models.Model):
+    title = models.CharField(max_length=255)
+    blog = models.CharField(max_length=1500)
+
+    def __str__(self):
+        return self.title
+
+class Contact(models.Model):
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    company = models.CharField(max_length=50)
+    phone = PhoneNumberField(blank=True)
+    email = models.EmailField()
+    topic = models.CharField(max_length=250)
+    message = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
